7feaf3cde5f1048e7e566302ca70a965823075ec
Dominik Przepióra <domin4815@gmail.com>
2018-04-07 17:54:18 +0200

    initial

---
+++ cmake-build-debug/CMakeFiles/multicast.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multicast.dir/main.c.o"
+  "multicast.pdb"
+  "multicast"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/multicast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
